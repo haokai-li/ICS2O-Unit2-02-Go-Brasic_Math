8a5aa4f3ac881ea0fe557e2ba8c3f59e21e1f7d1
haokaili <>
2021-05-06 20:49:17 +0000

    revise4

--- basicMath.go
+++ basicMath.go
@@ -6,12 +6,12 @@
 package main
 
 import (
-  "fmt"
-)
-
+	"fmt"
+	)
+	
 func main() {
-  // This function does Brasic_Math
-  fmt.Println("The dimensions of a rectangle are: 5cm and 3cm")
-  fmt.Println("The area is:", (3 * 5), "cm²")
-  fmt.Println("The perimeter is:", (2 * (3 + 5)), "cm")
+	// This function does Brasic_Math
+	fmt.Println("The dimensions of a rectangle are: 5cm and 3cm")
+	fmt.Println("The area is:", (3 * 5), "cm²")
+	fmt.Println("The perimeter is:", (2 * (3 + 5)), "cm")
 }
